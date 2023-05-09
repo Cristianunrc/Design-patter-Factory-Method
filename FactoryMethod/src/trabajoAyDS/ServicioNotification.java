da68951cce962cff2453ecca23f8e58d4f878587
MisterGena <genarosalomone@gmail.com>
2023-05-09 15:38:30 -0300

    pdf

--- FactoryMethod/src/trabajoAyDS/ServicioNotification.java
+++ FactoryMethod/src/trabajoAyDS/ServicioNotification.java
@@ -3,7 +3,7 @@ package trabajoAyDS;
 public class ServicioNotification {
 	public static void main(String[] args) {
 		NotificationFactory nf = new NotificationFactory();
-		Notification n = nf.crearNotificaction("WHATSAPP"); // new SlackNotification();
-		n.notificationUsuario(); // invoca al metodo de SlackNotification()
+		Notification n = nf.crearNotificaction("WHATSAPP"); // new WhatsAppNotification();
+		n.notificationUsuario(); // invoca al metodo de WhatsAppNotification()
 	}
 }
